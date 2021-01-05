4e2127d705f570da96ece8fa0682ae41d28b0253
Dave Lockhart <dlockhart@users.noreply.github.com>
2021-01-05 16:28:35 -0500

    the release name needs to contain "v"

--- incremental-release/create-release.js
+++ incremental-release/create-release.js
@@ -1,6 +1,6 @@
 const { Octokit } = require('@octokit/rest');
 
-const release = process.argv[2];
+const release = `v${process.argv[2]}`;
 const [owner, repo] = process.env['GITHUB_REPOSITORY'].split('/');
 
 const octokit = new Octokit({
