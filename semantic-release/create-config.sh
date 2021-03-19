a0bd65d237e42bfc8cc9c9ec5d24fe9fbeb8fbbe
Dave Lockhart <dlockhart@users.noreply.github.com>
2021-03-19 13:31:37 -0400

    add support for releasing from "main" branch (#21)

--- semantic-release/create-config.sh
+++ semantic-release/create-config.sh
@@ -6,6 +6,17 @@ else
 fi
 cat >$FILE_PATH <<EOL
 {
+  "release": {
+    "branches": [
+      "+([0-9])?(.{+([0-9]),x}).x",
+      "main",
+      "master",
+      "next",
+      "next-major",
+      {name: "beta", prerelease: true},
+      {name: "alpha", prerelease: true}
+    ]
+  },
   "plugins": [
     "@semantic-release/commit-analyzer",
     "@semantic-release/github",
