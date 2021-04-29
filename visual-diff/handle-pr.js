db1eae8fb2cee57da162a0bfd37880c8f5f81c00
Stacey Van Herk <Stacey.VanHerk@D2L.com>
2021-04-29 11:23:07 -0400

    Make opening the goldens PR as a draft PR a configurable option

--- visual-diff/handle-pr.js
+++ visual-diff/handle-pr.js
@@ -87,7 +87,7 @@ async function handlePR() {
 			title: prNum ? `Updating Visual Diff Goldens for PR ${prNum}` : `Updating Visual Diff Goldens for Branch ${sourceBranchName}`,
 			head: `refs/heads/${goldensBranchName}`,
 			base: `refs/heads/${sourceBranchName}`,
-			draft: true,
+			draft: process.env['DRAFT_PR'].toLowerCase() === 'false' ? false : true,
 			body: createPRBody()
 		});
 		goldenPrNum = newPR.data.number;
